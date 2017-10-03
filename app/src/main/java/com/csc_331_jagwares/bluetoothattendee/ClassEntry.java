77a131a15c96cae5248e625bd92013c196ad35e1
fl1721 <fl1721@jagmail.southalaba.edu>
2017-10-02 22:08:57 -0500

    Updated the class title and updated name of files. Added empty basic fragments

---
+++ app/src/main/java/com/csc_331_jagwares/bluetoothattendee/ClassEntry.java
@@ -0,0 +1,16 @@
+package com.csc_331_jagwares.bluetoothattendee;
+
+/**
+ * Created by fzlor on 10/2/2017.
+ */
+
+public class ClassEntry {
+
+    public String name;
+    public String location;
+
+    public ClassEntry(String name, String location){
+        this.name = name;
+        this.location = location;
+    }
+}
