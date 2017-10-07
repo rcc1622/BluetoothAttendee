e6555ba03531d2643ff3f68ceec5c767a24bb6e1
aad1621 <aad1621@jagmail.southalabama.edu>
2017-10-07 15:51:16 -0500

    Update registration navigation
    Added a StudentEntry class, adpaters, fragments, and layout files for the RegistrationFragment.

--- app/src/main/java/com/csc_331_jagwares/bluetoothattendee/RegistrationFragment.java
+++ app/src/main/java/com/csc_331_jagwares/bluetoothattendee/RegistrationFragment.java
@@ -3,9 +3,15 @@ package com.csc_331_jagwares.bluetoothattendee;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 
 /**
@@ -13,17 +19,72 @@ import android.view.ViewGroup;
  */
 public class RegistrationFragment extends Fragment {
 
+    // Holds all students from database.
+    private ArrayList<StudentEntry> studentEntries;
 
     public RegistrationFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Set title of the fragment to "Register Students".
+        getActivity().setTitle("Register Students");
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        // Populate list view with students.
+        // This array would come from the database.
+        studentEntries = new ArrayList<>();
+        StudentEntry student1 = new StudentEntry("Alex Dudenhoeffer", "J00123456");
+        StudentEntry student2 = new StudentEntry("Steven Mauseth", "J00111111");
+        StudentEntry student3 = new StudentEntry("Robert Cox", "J00222222");
+        StudentEntry student4 = new StudentEntry("Kaitlyn Gaiger", "J00333333");
+        StudentEntry student5 = new StudentEntry("Fernando Lorenzo", "J00444444");
+
+        studentEntries.add(student1);
+        studentEntries.add(student2);
+        studentEntries.add(student3);
+        studentEntries.add(student4);
+        studentEntries.add(student5);
+
+        // Add the studentes from the ArrayList to the ListView.
+        populateListView(view, studentEntries);
+
+        // Listen for a ListView entry selection.
+        registerClickCallback(view);
+
+        return view;
     }
 
+    private void populateListView(View view, ArrayList<StudentEntry> students) {
+        // Create the adapter to convert the array to views
+        StudentEntryAdapter adapter = new StudentEntryAdapter(getContext(), students);
+
+        // Attach the adapter to a ListView
+        ListView lvStudentList = view.findViewById(R.id.lvStudentList);
+        lvStudentList.setAdapter(adapter);
+    }
+
+    private void registerClickCallback(View view) {
+        ListView list = view.findViewById(R.id.lvStudentList);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                StudentEntry entry = studentEntries.get(position);
+
+/*                Fragment fragment = new ClassFragment();
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("Student Entry", entry);
+                fragment.setArguments(bundle);
+
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.mainLayout, fragment);
+                ft.addToBackStack(null);
+                ft.commit();*/
+            }
+        });
+    }
 }
