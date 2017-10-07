b7112fe9463cca7dd0514233e8dd10dc389c9972
fl1721 <fl1721@jagmail.southalaba.edu>
2017-10-07 16:10:26 -0500

    Added titles to the other fragments

--- app/src/main/java/com/csc_331_jagwares/bluetoothattendee/ClassRosterFragment.java
+++ app/src/main/java/com/csc_331_jagwares/bluetoothattendee/ClassRosterFragment.java
@@ -22,6 +22,7 @@ public class ClassRosterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        getActivity().setTitle("Class Roster");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_class_roster, container, false);
     }
