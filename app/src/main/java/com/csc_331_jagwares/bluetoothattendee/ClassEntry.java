32cc4b8748dc2ed60d5583798fbc2495f89b2fb1
aad1621 <aad1621@jagmail.southalabama.edu>
2017-10-07 18:23:51 -0500

    Update ClassesFragment
    Add TableLayout to each ListView entry.

--- app/src/main/java/com/csc_331_jagwares/bluetoothattendee/ClassEntry.java
+++ app/src/main/java/com/csc_331_jagwares/bluetoothattendee/ClassEntry.java
@@ -8,38 +8,74 @@ import android.os.Parcelable;
 public class ClassEntry implements Parcelable {
 
     private String name;
+    private String title;
     private String location;
+    private String days;
+    private String time;
 
-    public ClassEntry(String name, String location){
+    public ClassEntry(String name, String title, String location, String days, String time){
         this.name = name;
+        this.title = title;
         this.location = location;
+        this.days = days;
+        this.time = time;
     }
 
     void setName(String name) {
         this.name = name;
     }
 
+    void setTitle(String title) {
+        this.title = title;
+    }
+
     void setLocation(String location) {
         this.location = location;
+    }
+
+    void setDays(String days) {
+        this.days = days;
+    }
+
+    void setTime(String time) {
+        this.time = time;
     }
 
     String getName(){
         return this.name;
     }
 
+    String getTitle(){
+        return this.title;
+    }
+
     String getLocation(){
         return this.location;
     }
 
+    String getDays(){
+        return this.days;
+    }
+
+    String getTime(){
+        return this.time;
+    }
+
     protected ClassEntry(Parcel in) {
         name = in.readString();
+        title = in.readString();
         location = in.readString();
+        days = in.readString();
+        time = in.readString();
     }
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(name);
+        dest.writeString(title);
         dest.writeString(location);
+        dest.writeString(days);
+        dest.writeString(time);
     }
 
     @Override
