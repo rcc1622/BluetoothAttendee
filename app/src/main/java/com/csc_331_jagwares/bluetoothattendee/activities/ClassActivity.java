5b34103ae29b9a98f7e47ca9077871b7188c92bd
aad1621 <aad1621@jagmail.southalabama.edu>
2017-10-08 20:21:08 -0500

    Setup Bluetooth adapter
    Add a bluetooth adapter and request the user to enable Bluetooth if it is disabled.

--- app/src/main/java/com/csc_331_jagwares/bluetoothattendee/activities/ClassActivity.java
+++ app/src/main/java/com/csc_331_jagwares/bluetoothattendee/activities/ClassActivity.java
@@ -1,5 +1,6 @@
 package com.csc_331_jagwares.bluetoothattendee.activities;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import com.csc_331_jagwares.bluetoothattendee.models.Class;
 public class ClassActivity extends AppCompatActivity {
 
     private Class classEntry;
+    private static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,18 @@ public class ClassActivity extends AppCompatActivity {
             classEntry = intent.getExtras().getParcelable("classEntry");
         } catch(Exception e) {
             e.printStackTrace();
+        }
+
+        // Setup Bluetooth adapter.
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        } else {
+            // Request to enable Bluetooth if it is disabled.
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
 
         // Open ClassFragment initially.
